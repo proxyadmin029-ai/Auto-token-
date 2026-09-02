491e93b09354081ac04004961c83131617ed426f
proxyadmin029-ai <proxyadmin029@gmail.com>
2026-09-02 11:14:59 +0530

    Add files via upload

--- bot.py
+++ bot.py
@@ -60,7 +60,7 @@ BOT_NAME = "<b>𝗔𝗡𝗬 𝗔𝗨𝗧𝗢 𝗕𝗢𝗧</b>"
 # FORCE JOIN CHANNEL
 # ============================
 CHANNEL_USERNAME = "@Ro999itoekng"
-CHANNEL_URL = "https://t.me/+MIgVVTfH2-w4Y2U1
+CHANNEL_URL = "https://t.me/+MIgVVTfH2-w4Y2U1"
 
 # ============================
 # USER CONFIG – PERSISTENT VOLUME FIX
